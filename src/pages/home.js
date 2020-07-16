353b78541a2f56386f8475fabcacc889dc514bfa
ollijantunen <32096381+ollijantunen@users.noreply.github.com>
2020-07-16 14:56:08 +0300

    bug fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ let home = `
 <h1>Welcome to this questionnaire!</h1>
 
 <p class="lead">This page is a single page application</p>
-<p><a href="#about"><button type="button" id="toApp" class="btn btn-primary">How do you feel?</button></a>
+<p><a href="#questionnaire"><button type="button" id="toApp" class="btn btn-primary">How do you feel?</button></a>
 
 </div>
 `;
